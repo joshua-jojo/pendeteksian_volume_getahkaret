46a09449cfdb3ca35c8a12fe1dae0dee6321c3fd
joshua-jojo <joshuasitompul2000@gmail.com>
2022-06-19 21:59:37 +0700

    hcsr final

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,13 +1,39 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 5, 4);
+#define trigger 2
+#define echo 3
+
+long duration;
+int distance;
 
 void setup() {
+  Serial.begin(57600);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
   lcd.begin();
 }
 
-void loop(){
-  lcd.setCursor(0,0);
-  lcd.print("TEST LCD i2C");
-  lcd.setCursor(0,1);
-  lcd.print("KelasRobot.com");
+
+void loop() {
+  String jarak = String(hcsr());
+  tampil_lcd("Jarak : " + jarak+" CM");
+  Serial.println(hcsr());
+  delay(500);
+  
+}
+
+int tampil_lcd(String data) {
+  lcd.setCursor(0, 0);
+  lcd.print(data);
+}
+
+int hcsr() {
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  duration = pulseIn(echo, HIGH);
+  distance = duration * 0.034 / 2;
+  return distance;
 }
